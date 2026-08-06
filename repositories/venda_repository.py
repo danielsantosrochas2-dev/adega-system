ca55736638f03c19e4b52c5c8d0f91b1a008004c
Daniel Santos <danielsantosrochas2@gmail.com>
2026-08-06 01:00:17 -0300

    Update no venda_repository
    criado mais algumas funções no repository de venda

--- repositories/venda_repository.py
+++ repositories/venda_repository.py
@@ -1,6 +1,6 @@
 from database.conexao import conectar
 
-# Criar venda
+# Criar venda, chama a tabela vendas
 
 def nova_venda(usuario_id, data_hora, valor_total, forma_pagamento, status, valor_desconto):
 
@@ -20,4 +20,80 @@ def nova_venda(usuario_id, data_hora, valor_total, forma_pagamento, status, valo
 
     return venda_id
 
-# Adicionar item a venda
+# Adicionar item na venda chama a tabela iten_venda
+
+def adicionar_item_na_venda(venda_id, produto_id, quantidade, preco_unitario, subtotal, tipo_item, desconto):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    INSERT INTO itens_venda (venda_id, produto_id, quantidade, preco_unitario, subtotal, tipo_item, desconto)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (venda_id, produto_id, quantidade, preco_unitario, subtotal, tipo_item, desconto))
+
+    conexao.commit()
+
+    item_venda_id = cursor.lastrowid
+
+    conexao.close()
+
+    return item_venda_id
+
+# Remover item da venda 
+
+def remover_item_da_venda(item_venda_id):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    DELETE FROM itens_venda
+    WHERE id = ?
+    """, (item_venda_id,))
+
+
+    conexao.commit()
+
+    apagou = cursor.rowcount > 0
+
+    conexao.close()
+
+    return apagou
+
+# Finalizar a venda
+
+def finalizar_venda(venda_id, status):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    UPDATE vendas
+    SET status = ?
+    WHERE id = ?
+    """, (status, venda_id))
+
+    conexao.commit()
+    conexao.close ()
+
+    return True
+
+# Cancelar Venda
+
+def cancelar_venda(venda_id):
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    UPDATE vendas
+    SET status = ?
+    WHERE id = ?
+    """, ("Cancelada", venda_id))
+
+    conexao.commit()
+    conexao.close ()
+
+    return True
+
